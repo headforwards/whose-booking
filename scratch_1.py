4e2fbf0edf89e0294b7038ff392b2e787240d7e2
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-07-01 15:48:24 +0100

    Added Room and Index Route

--- scratch_1.py
+++ scratch_1.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/<room>")
+def bookings(room):
+    return render_template('room.html', name=room)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('index.html')
