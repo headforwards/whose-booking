758f9da24b094090add8cffff50dd3cd739577aa
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-07-02 14:15:33 +0100

    Added authentication call

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, redirect, request, url_for
-from auth_helper import get_sign_in_url, get_token_from_code
+from auth_helper import get_token
+from graph_helper import get_user
 
 app = Flask(__name__)
 app.secret_key='ajdhjkaghsdfhsdaf'
@@ -10,26 +11,11 @@ def bookings(room):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    if session['token'] == '':
+        get_token()
+    token = session['token']
+    return render_template('index.html', displaytoken=token)
 
-@app.route("/sign-in")
-def sign_in():
-    # Get the sign-in URL
-    sign_in_url, state = get_sign_in_url()
-    # Save the expected state so we can validate in the callback
-    session['auth_state'] = state
-    # Redirect to the Azure sign-in page
-    return redirect(sign_in_url)
-
-@app.route("/callback")
-def callback():
-    # Get the state saved in session
-    expected_state = session.pop('auth_state', '')
-    # Make the token request
-    token = get_token_from_code(request.url, expected_state)
-    #Temporary! Save the response in an error so it's displayed
-    session['flash_error'] = { 'message': 'Token retrieved', 'debug': format(token) }
-    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
