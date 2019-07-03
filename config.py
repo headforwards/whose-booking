0c08a6ebea110e8694f8b2f592aa14e55b671a7e
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-07-03 14:17:12 +0100

    Possibly fixed issue

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import yaml
 import os
 
-os.environ['app_id'] = ''
+'''os.environ['app_id'] = ''
 os.environ['app_secret'] = ''
 os.environ['tenant_id'] = ''
 os.environ['user_id'] = ''
-
+'''
 def setup():
     try:
         stream = open('oauth_settings.yml', 'r')
