a2c4829ea89519437d8711e2a332643212221801
Barney Nicholls <barney.nicholls@bluetoad.net>
2019-07-03 13:57:39 +0100

    Possibly fixed issue

--- graph_helper.py
+++ graph_helper.py
@@ -12,7 +12,12 @@ def get_rooms():
     url = '{0}/users/{1}/{2}'.format(graph_url, user_id, 'findrooms')
     headers = {"Authorization": "Bearer {0}".format(session['token'])}
     response = requests.get(url, headers=headers).json()
-    return response['value']
+    if 'value' in response:
+        return response['value']
+    else:
+        print(response)
+        return []
+
 
 def get_meetings(room):
     today = datetime.date.today().strftime("%Y-%m-%d")
