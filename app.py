8b5448cd3b0d30ccfa600a79f0887b40726fc086
Dave Longman <dave@Daves-Work-Mac.local>
2019-07-08 11:45:29 +0100

    Added pic room links

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ roomUrls = {
     "opie.liskov@headforwards.com": "http://outlook.office365.com/owa/calendar/4c9496f85f904f7286fc4fb2d2365a62@headforwards.com/5f4c4a8c13d64de68deee8fa642cba3517947868822812662980/calendar.html", #liskov
     "opie.newman@headforwards.com": "http://outlook.office365.com/owa/calendar/2e4e6143c7ff4de9ab6e5aeae3aea647@headforwards.com/274393823c1f4f5f98851bf2247f7e7c9740793119705658626/calendar.html", #newman
     "opie.hamilton@headforwards.com": "http://outlook.office365.com/owa/calendar/e49354d94fbf484f9b2c85e62ab925a2@headforwards.com/5a9a650be6304edd97e48833dfc556c713049660601462722245/calendar.html", #hamilton
-    "pic.s18@headforwards.com": "#", #s18
-    "pic.green@headforwards.com": "#", #green
-    "pic.meetingrooms@headforwards.com": "#", #charged
+    "pic.s18@headforwards.com": "http://outlook.office365.com/owa/calendar/cef28e595bae44e29c6f798c341c6923@headforwards.com/efc32ddb717c428580c88804431ae5ba6986522120498447319/calendar.html", #s18
+    "pic.green@headforwards.com": "http://outlook.office365.com/owa/calendar/90a73bc735a24e1a80d8353ea2eb85c1@headforwards.com/98b70a3fc9b1443782a72ffc26ac53b56222321996911569098/calendar.html", #green
+    "pic.meetingrooms@headforwards.com": "http://outlook.office365.com/owa/calendar/d1250939d2be405582a57a6e932d58e1@headforwards.com/eb2b759d0d5a42919aec15effe6ea2ea15405909583009543192/calendar.html", #charged
 }
 
 
